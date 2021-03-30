6728d191fc479649d1ecfc73aa78560247bfe56a
B-G Seo <bumku1234@gmail.com>
2021-03-30 11:01:26 +0900

    inner class without this

--- app/src/main/java/com/example/buttonwiinnerclass/MainActivity.java
+++ app/src/main/java/com/example/buttonwiinnerclass/MainActivity.java
@@ -20,12 +20,10 @@ public class MainActivity extends AppCompatActivity {
         mButton1 = findViewById(R.id.button1);
         mTextView1 = findViewById(R.id.textView1);
 
-        mButton1.setOnClickListener(new MyOnClickListener(this));
+        mButton1.setOnClickListener(new MyOnClickListener());
     }
 
     private class MyOnClickListener implements View.OnClickListener {
-        public MyOnClickListener(MainActivity mainActivity) {
-        }
 
         @Override
         public void onClick(View v) {
